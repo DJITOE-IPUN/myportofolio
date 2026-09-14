1824033e46b7e6956a3ea15277c10fe0fa940031
Michael Evan Putra Nugroho <michaelevanpn@gmail.com>
2026-09-14 18:47:44 +0700

    feat: add admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from main.models import Experience, Education, Award
 
-# Register your models here.
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'organization', 'category', 'date_range', 'is_ongoing')
+    list_filter = ('category', 'is_ongoing')
+    search_fields = ('title', 'organization', 'description')
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('institution', 'degree', 'field_of_study', 'date_range')
+    list_filter = ('degree',)
+    search_fields = ('institution', 'field_of_study')
+
+@admin.register(Award)
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'issuer', 'category', 'date_awarded')
+    list_filter = ('category',)
+    search_fields = ('title', 'issuer')
