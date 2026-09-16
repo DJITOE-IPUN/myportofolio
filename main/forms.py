c7f9bbdc49baf7a0c0c6e4e9259451c23709d2b1
Michael Evan Putra Nugroho <michaelevanpn@gmail.com>
2026-09-16 14:31:43 +0700

    feat: add form and data delivery

---
+++ main/forms.py
@@ -0,0 +1,101 @@
+from django.forms import ModelForm, TextInput, Textarea, URLInput
+
+from main.models import Education, Project
+
+# class EducationForm(ModelForm):
+#     class Meta:
+#         model = Education
+#         fields = [
+#             "institution",
+#             "degree",
+#             "field_of_study",
+#             "date_range",
+#             "description",
+#         ]
+
+#         labels = {
+#             "institution": "Educational Institution",
+#             "degree": "Education Level",
+#             "field_of_study": "Field of Study",
+#             "date_range": "Study Period",
+#             "description": "Description",
+#         }
+
+#         widgets = {
+#             "institution": TextInput(
+#                 attrs={
+#                     "placeholder": "School, University, Academy, etc",
+#                     "maxlength": 255,
+#                 }
+#             ),
+#             "degree": Textarea(
+#                 attrs={
+#                     "placeholder": "Undergraduate, Diploma, Bachelor, etc",
+#                     "rows": 3,
+#                 }
+#             ),
+#             "field_of_study": TextInput(
+#                 attrs={
+#                     "placeholder": "Study Program",
+#                 }
+#             ),
+#             "date-range": URLInput(
+#                 attrs={
+#                     "placeholder": "(year)-(year)",
+#                 }
+#             ),
+#             "description": URLInput(
+#                 attrs={
+#                     "placeholder": "Describe your educational institute and/or your activities in there",
+#                 }
+#             ),
+#         }
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            "title",
+            "description",
+            "tech_stack",
+            "project_url",
+            "project_image_url",
+        ]
+
+        labels = {
+            "title": "Nama Proyek",
+            "description": "Deskripsi Proyek",
+            "tech_stack": "Teknologi yang Digunakan",
+            "project_url": "URL Proyek",
+            "project_image_url": "URL Gambar Proyek",
+        }
+
+        widgets = {
+            "title": TextInput(
+                attrs={
+                    "placeholder": "Portfolio Website",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "Ceritakan Proyekmu",
+                    "rows": 3,
+                }
+            ),
+            "tech_stack": TextInput(
+                attrs={
+                    "placeholder": "Django, Python, HTML, CSS",
+                }
+            ),
+            "project_url": URLInput(
+                attrs={
+                    "placeholder": "https://github.com/kakBurhan/burhanquestv4",
+                }
+            ),
+            "project_image_url": URLInput(
+                attrs={
+                    "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
+                }
+            ),
+        }
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_awards, show_education, show_experience, show_main
+from main.views import show_awards, show_education, show_experience, show_main, show_projects, add_education, add_project, delete_project, get_projects_json
 
 app_name = "main"
 
@@ -9,4 +9,9 @@ urlpatterns = [
     path("experience/", show_experience, name="show_experience"),
     path('education/', show_education, name='show_education'),
     path('awards/', show_awards, name='show_awards'),
+    path("projects/", show_projects, name="show_projects"),
+    # path("education/add/", add_education, name="add_education"),
+    path("projects/add/", add_project, name="add_project"),
+    path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
+    path("api/projects/", get_projects_json, name="get_projects_json"),
 ]
